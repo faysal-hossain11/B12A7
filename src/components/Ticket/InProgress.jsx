e3c6ea0a9ee331811460c7908e7af01d8c5a2f00
faysal-hossain11 <devfaysalbd@gmail.com>
2025-09-27 14:09:25 +0600

    fix the deploy fetching issue

--- src/components/Ticket/InProgress.jsx
+++ src/components/Ticket/InProgress.jsx
@@ -5,14 +5,14 @@ const InProgress = ({ inProgress }) => {
     return (
         <div className='flex justify-between h-[270px]  bg-gradient-to-br from-[#632ee3] to-[#9f62f2] rounded-lg relative'>
             <div>
-                <img className='h-full w-full' src="/public/assets/vector1.png" alt="" />
+                <img className='h-full w-full' src="/assets/vector1.png" alt="" />
             </div>
             <div className='flex justify-center flex-col items-center min-w-[200px] text-2xl text-white'>
                 <div className="text-2xl">In-Progress</div>
                 <div className="text-[40px] font-semibold mt-4">{inProgress?.length}</div>
             </div>
             <div>
-                <img className='h-full w-full' src="/public/assets/vector2.png" alt="" />
+                <img className='h-full w-full' src="/assets/vector2.png" alt="" />
             </div>
         </div>
     );
